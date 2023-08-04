39c5fef6bffb85e3115de7613db4a2cf5c03ca32
Omar Gonzalez <gomarzalez@gmail.com>
2023-08-04 13:36:02 -0700

    commit basic outline

---
+++ ROI-Calc.py
@@ -0,0 +1,27 @@
+# create a class to handle the calculations for Return on Investment (ROI).
+# The ROI will be calculated using the formula:
+
+# ROI = (Net Profit / Initial Investment) * 100
+
+# Net Profit = Total Rental Income - Total Expenses
+# Initial Investment = Purchase Price + Closing Costs + Renovation Costs
+
+class RentalProperty:
+    def __init__(self, purchase_price, closing_costs, renovation_costs, rental_income, total_expenses):
+        pass
+
+    def calculate_net_profit(self):
+        pass
+
+    def calculate_initial_investment(self):
+        pass
+
+    def calculate_roi(self):
+        pass
+
+        if initial_investment == 0:
+            return 0
+
+        roi = (net_profit / initial_investment) * 100
+        return roi
+
